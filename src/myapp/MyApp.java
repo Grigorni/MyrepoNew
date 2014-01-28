0097767437830168e265a90e9662fdd95ab0ce42
morris <morris@morris-SATELLITE-PRO-C70-A>
2014-01-28 18:35:04 +0000

    version 2

--- src/myapp/MyApp.java
+++ src/myapp/MyApp.java
@@ -16,7 +16,7 @@ public class MyApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("version 2");
     }
     
 }
